5e2bc66b76d5d8d8656ec2693f6ec8257c4dcea4
Paulo Sergio <49497668+paulosergiocf@users.noreply.github.com>
2025-04-29 23:12:14 -0300

    created models

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from src.config.db_sessions import create_tables
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
 if __name__=='__main__':
-    pass
+    create_tables()
---
+++ src/config/db_sessions.py
@@ -0,0 +1,64 @@
+import sqlalchemy as sa
+from sqlalchemy.orm import sessionmaker
+from pathlib import Path
+from typing import Optional
+from sqlalchemy.orm import Session
+from sqlalchemy.future.engine import Engine
+from src.models.model_base import ModelBase
+import os
+
+__engine: Optional[Engine] = None
+
+def create_engine(sqlite: bool = False) -> Engine:
+    """
+        Cria Engine de conexão com a base de dados.
+    """
+    global __engine
+
+    if __engine:
+        return
+    
+    if sqlite:
+        arquivo_db = 'data/database.sqlite'
+        folder = Path(arquivo_db).parent
+        folder.mkdir(parents=True, exist_ok=True)
+
+        conn_str = f'sqlite:///{arquivo_db}'
+        __engine = sa.create_engine(url=conn_str, echo=False, connect_args={"check_same_thread":False})
+    
+    else:
+        conn_str = f'postgresql://{os.environ["USERPG"]}:{os.environ["PASSPG"]}@{os.environ["HOST"]}:{os.environ["PORT"]}/{os.environ["DATABASE"]}'
+
+        __engine = sa.create_engine(url=conn_str, echo=False)
+
+def create_session() -> Session:
+    """
+        Cria session de conexão com a base de dados.
+    """
+    
+    global __engine
+
+    if not __engine:
+        create_engine(sqlite=True)
+        
+    __session = sessionmaker(__engine, expire_on_commit=False, class_=Session)
+
+    session: Session = __session()
+
+    return session
+
+
+
+def create_tables() -> None:
+    """
+    Criar tabelas.
+    """
+
+    global __engine
+
+    if not __engine:
+        create_engine(sqlite=True)
+
+    from src.models import __all__models
+    ModelBase.metadata.drop_all(__engine)
+    ModelBase.metadata.create_all(__engine)
